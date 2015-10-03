f7a4494b1d276022ba6500ef14af0ae9d5a10487
Banu Dalamanli <banudalamanli@gmail.com>
2015-10-03 14:49:38 -0700

    combined items in cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003212503) do
+ActiveRecord::Schema.define(version: 20151003214431) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
